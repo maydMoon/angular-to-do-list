553da537fb05d842af1d6e118bd841209ce22d53
Maylla Krislainy Matos de Sousa <maylla.sousa@autotrac.com.br>
2024-04-25 14:39:13 -0300

    [CSS] Page List Component

--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -26,4 +26,9 @@ export class ListComponent {
 
     return this.#setListItems.set(this.#parseItems());
   }
+
+  public deleteAllItems(){
+    localStorage.removeItem('@my-list');
+    return this.#setListItems.set(this.#parseItems());
+  }
 }
