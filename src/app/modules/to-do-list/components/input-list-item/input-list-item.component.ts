c478dcda02b20f53e8df09fa0d540b9152486ca0
Maylla Krislainy Matos de Sousa <maylla.sousa@autotrac.com.br>
2024-04-25 14:40:50 -0300

    Filtering data - Update Checked

--- src/app/modules/to-do-list/components/input-list-item/input-list-item.component.ts
+++ src/app/modules/to-do-list/components/input-list-item/input-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
 
 // Interface
 import { IListItems } from '../../../interface/iListItems.interface';
@@ -13,4 +13,13 @@ import { IListItems } from '../../../interface/iListItems.interface';
 
 export class InputListItemComponent {
   @Input({ required: true }) public inputListItems: IListItems[] = [];
+
+  @Output() public outputUpdateItemCheckbox = new EventEmitter<{
+    id: string;
+    checked: boolean;
+  }>();
+
+  public updateItemCheckbox(id: string, checked: boolean) {
+    return this.outputUpdateItemCheckbox.emit({ id, checked });
+  }
 }
